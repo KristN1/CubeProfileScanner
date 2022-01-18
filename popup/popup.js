dbf0d9c7a92561c5326568cdd1878f61d0324ea1
KristN <kristian.kunc@gmail.com>
2022-01-18 23:26:38 +0100

    updated manifest to v3 for chromium browsers

--- popup/popup.js
+++ popup/popup.js
@@ -4,4 +4,12 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-chrome.tabs.executeScript({file: "/scanner.js"});
+async function getTabId() {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab.id;
+  }
+
+const tabId = getTabId().then (tabId => {
+    chrome.scripting.executeScript({files: ["/scanner.js"], target: {tabId: tabId, allFrames: true}})
+});
